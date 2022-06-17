d4b456e82b96682d21a96cb0e1cd553f347513ff
Rui Ma <marui1@ruijie.com.cn>
2022-06-17 10:05:59 +0800

    新增编排

--- release/docker/build.sh
+++ release/docker/build.sh
@@ -1,7 +1,7 @@
 echo "build jetbrains start"
 
-docker buildx build -t jetbrains:$1 --platform=linux/arm64,linux/amd64 -f release/docker/Dockerfile . --push
+docker buildx build -t toodo/jetbrains:$1 --platform=linux/arm64,linux/amd64 -f release/docker/Dockerfile . --push
 
-docker buildx build -t jetbrains:latest --platform=linux/arm64,linux/amd64 -f release/docker/Dockerfile . --push
+docker buildx build -t toodo/jetbrains:latest --platform=linux/arm64,linux/amd64 -f release/docker/Dockerfile . --push
 
 echo "build jetbrains finish"
